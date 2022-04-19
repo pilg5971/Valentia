b5741d3184e87d58631995e9fdce90b2c7f79f34
Trevor Pilgrim <tpilgrim@fishbowlsolutions.com>
2022-04-19 08:00:32 -0500

    Formatting for ImageCard

--- src/components/ImageGallery/ImageCard.jsx
+++ src/components/ImageGallery/ImageCard.jsx
@@ -2,8 +2,7 @@ function ImageCard(props) {
   return(
     <div className={props.ColumnSize}>
       <div className="card">
-        <img className="card-img-top" src={props.Image} alt="Card image"
-        />
+        <img className="card-img-top" src={props.Image} alt="Card image"/>
         <div className="card-body">
           <h2 className="card-title">{props.CardTitle}</h2>
         </div>
