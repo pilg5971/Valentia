46861a690a70b345b25efb768c8f2cd9c6eeb9f5
Trevor Pilgrim <tpilgrim@fishbowlsolutions.com>
2022-04-20 11:28:22 -0500

    Added Character Generator

--- src/components/routes/OneShot.jsx
+++ src/components/routes/OneShot.jsx
@@ -3,26 +3,20 @@ import "./page_styles.css"
 
 function OneShot() {
   return (
-    <div className="OneShot">
-      <div className="container">
-        <div className="row align-items-center my-5">
-          <div className="col-lg-7">
-            <a href="https://www.riskofrain.com/" target="_blank" rel="noreferrer">
-              <img
-                className="hover-card img-fluid rounded mb-4 mb-lg-0"
-                src=""
-                alt="Risk of Rain 2 Wallpaper"
-              />
-            </a>
+    <div id="one-shot">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 col-sm-12 py-4 pl-4 char-image">
+            <iframe src="https://tetra-cube.com/dnd/dnd-char-gen.html" 
+              frameborder="2"
+              title="Character Image & Story">
+            </iframe>
           </div>
-          <div className="col-lg-5">
-            <h1 className="font-weight-light">OneShot</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+          <div className="col-md-6 col-sm-12 py-4 pr-4 char-sheet">
+            <iframe src="https://levi-blodgett.github.io/dnd-char-generator/" 
+              frameborder="2"
+              title="Character Stats">
+            </iframe>
           </div>
         </div>
       </div>
