a1faf64bb704b8e1f246412b1480486e80b43628
Trevor Pilgrim <tpilgrim@fishbowlsolutions.com>
2022-04-20 09:01:16 -0500

    Removed alt tag for images

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,7 +1,7 @@
 function ArticleCard(props) {
   return(
     <div className="card">
-      <img src={props.Image} className="card-img-top" alt="Article Image" />
+      <img src={props.Image} className="card-img-top" />
       <div className="card-body text-center">
         <h5 className="card-title">{props.CardTitle}</h5>
         <p className="card-text ">{props.CardBody}</p>
